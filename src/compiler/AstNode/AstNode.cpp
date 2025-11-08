b2ac7bc9f1601bd2df1a2c25c73a15550d03547b
Zach Preston <zachattack6089@gmail.com>
2025-11-07 18:15:33 -0800

    Start from scratch

--- src/compiler/AstNode/AstNode.cpp
+++
@@ -1,5 +0,0 @@
-#include "AstNode.hpp"
-
-AstNode::AstNode(const Value::Ptr v) : type(VALUE), value(v), children({}) {}
-
-AstNode::AstNode(const std::vector<AstNode::Ptr> c) : type(FUNCTION_CALL), value(nullptr), children(c) {}
--- src/compiler/Token/Token.cpp
+++
@@ -1,3 +0,0 @@
-#include "Token.hpp"
-
-Token::Token(TokenId i, std::string r) : id(i), raw_text(r) {}
